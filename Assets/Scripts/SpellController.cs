0cb6c8920898b2fed787dd15a748122f713b3fcd
ghostinajar <info@miark.ca>
2018-04-17 20:58:41 -0700

    First Spell (till soil)
     Scripting:
      -Researched Event Delegate pattern in current documentation
      -Created a SpellController script to hold all spell functions and control spell1 and spell2 delegates
      -Completed Till script and hook up to player left click Delegate

---
+++ Assets/Scripts/SpellController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SpellController : MonoBehaviour {
+
+	public delegate void SpellDelegate (Tilemap tileMap, TileBase tileBase, Vector3Int position);
+	public SpellDelegate spell1;
+	public SpellDelegate spell2;
+	public Vector3 mousePos;
+	public Vector3 mousePosWorld;
+	public Vector3Int clickedCell;
+	public Tilemap tileMap;
+	public TileBase tileBase;
+	public GameObject testObj;
+	void Start () {
+		spell1 = TillSoil;
+	}
+
+	void Update () {
+
+	}
+
+	public void TillSoil(Tilemap tileMap, TileBase tileBase, Vector3Int position) {
+		tileMap.SetTile (position, tileBase);
+		tileMap.RefreshTile (position);
+		//GameObject.Instantiate (testObj, position, Quaternion.identity);
+	}
+
+
+	void OnGUI() {
+		Camera  c = Camera.main;
+		bool fire1 = Input.GetButton ("Fire1");
+		if (fire1) {
+			mousePos = Input.mousePosition;
+			mousePosWorld = c.ScreenToWorldPoint (mousePos);
+			clickedCell = tileMap.WorldToCell (mousePosWorld);
+			spell1 (tileMap, tileBase, clickedCell);
+		}
+	}
+
+}
